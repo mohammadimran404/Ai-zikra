70da4c94aaf3abd261e45d560c2e20a724a202b2
mohammadimran404 <mohammad12345imran54321@gmail.com>
2026-03-26 23:49:54 +0530

    Add files via upload

---
+++ Client/Src/App.tsx
@@ -0,0 +1,28 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Chat from "@/pages/chat";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Chat} />
+      <Route component={Chat} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
